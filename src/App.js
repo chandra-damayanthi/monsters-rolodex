940d9bbab4fe5e2aa4e9afb8f760b27596ba287a
Chandra Damayanthi <cdamayanthi819@gmail.com>
2023-11-08 02:13:11 +0530

    Add state for count

--- src/App.js
+++ src/App.js
@@ -5,22 +5,21 @@ import { Component } from 'react'
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Sate: <code>const i = 0</code>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>Click me!</button>
         </header>
       </div>
     );
